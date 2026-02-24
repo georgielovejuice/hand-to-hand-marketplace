27f7648a914f9a3e5257e3083c8c255e98e7058a
Pawikan Boonnaum <160022244+FriedHamCheese@users.noreply.github.com>
2026-02-24 16:30:07 +0700

    Added loading state for BrowsePage

--- frontend/src/pages/BrowsePage.js
+++ frontend/src/pages/BrowsePage.js
@@ -6,16 +6,25 @@ import axios from "axios";
 export default function BrowsePage({apiURL, userID, setViewingItemID}) {
     const [items, setItems] = useState([]);
     const [searchText, setSearchText] = useState('');
+    const [waitingForResponse, setWaitingForResponse] = useState(false);
 
     const fetchItems = async () => {
+      function receivedResponse(response){
+        setWaitingForResponse(false);
+        setItems(response.data.items);    
+      }
+        
       try {
-        const res = await axios.post("http://localhost:5001/api", {
+        const responsePromise = axios.post("http://localhost:5001/api", {
             requestType: "getItems",
             searchBarText: searchText,
             query: {},
             userID: userID,
-        })
-        setItems(res.data.items)
+        });
+        
+        setWaitingForResponse(true);
+        responsePromise.then(receivedResponse, alert);
+        
       } catch (error) {
         console.error(error)
       }
@@ -27,15 +36,22 @@ export default function BrowsePage({apiURL, userID, setViewingItemID}) {
   }, [searchText])
 
   return (
-    <>
     <div className='min-h-screen bg-orange-200'>
-        <SearchBar setSearchText={setSearchText} fetchItems={fetchItems}></SearchBar>
-        <div className='flex justify-center flex-wrap gap-9 p-6'>
-        {items.map((item) => (
-          <Container item={item} setViewingItemID={setViewingItemID}/>
-        ))}
+      {
+        waitingForResponse && 
+        <div className="absolute flex justify-center items-center w-[100vw] h-[100vh] bg-[rgba(255,255,255,0.9)]">
+            <p className="inline-block font-[500] text-[24px] text-[#FF9E21]">
+              Thinking of items for you...
+            </p>
+        </div>
+      }
+      <SearchBar setSearchText={setSearchText} fetchItems={fetchItems}></SearchBar>
+      <div className='flex justify-center flex-wrap gap-9 p-6'>
+      {items.map((item) => (
+        <Container item={item} setViewingItemID={setViewingItemID}/>
+      ))}
       </div>
+      
     </div>
-    </>
   )
 }
